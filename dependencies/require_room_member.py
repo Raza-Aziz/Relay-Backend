c594a620ae2ea287964485996d6dd19049803aba
Raza Aziz <razaaziz9191@gmail.com>
2026-03-24 22:44:23 +0500

    feat: Make a chat room dependency

---
+++ dependencies/require_room_member.py
@@ -0,0 +1,26 @@
+from uuid import UUID
+
+from fastapi import Depends, Path
+from starlette.exceptions import HTTPException
+from starlette.status import HTTP_403_FORBIDDEN
+
+from db.client import client
+from dependencies.get_current_user import get_current_user
+
+
+def require_room_member(
+    room_id: UUID = Path(...), current_user=Depends(get_current_user)
+):
+    result_rooms = (
+        client.table("room_member")
+        .select("*")
+        .eq("room_id", str(room_id))
+        .eq("user_id", current_user["id"])  # current_user is a dict
+    ).execute()
+
+    if not result_rooms.data:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="User not member of this room"
+        )
+
+    return result_rooms.data[0]
